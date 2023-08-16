04905419aba5cecc03bbf94321822c79a32d1464
Tadiwanashe David Pfachi <pfachitadiwanashedavid@gmail.com>
2023-08-16 07:23:31 +0200

    grab dat from form

---
+++ includes/login.inc.php
@@ -0,0 +1,20 @@
+<?php
+
+if(isset($_POST["submit"])){
+
+    // grabbing data from form
+    $uid = $_POST["uid"];
+    $pwd = $_POST["pwd"];
+    $pwdrepeat = $_POST["pwdrepeat"];
+    $email = $_POST["email"];
+
+    //instantiate signupcontroller class
+
+
+    //Running error handlers and user signup
+
+
+    //going back to front page
+
+    
+}
